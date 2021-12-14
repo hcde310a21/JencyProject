9b7ba62e45d1a92c1761edd1a97e6416dbe0a577
jencyc1 <91704995+jencyc1@users.noreply.github.com>
2021-12-13 16:09:42 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import urllib.parse, urllib.request, urllib.error, json
+from flask import Flask, render_template, request
+import random
+import pip
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+
+def safe_get(url):
+    try:
+        return urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        print("The server couldn't fulfill the request." )
+        print("Error code: ", e.code)
+    except urllib.error.URLError as e:
+        print("We failed to reach a server")
+        print("Reason: ", e.reason)
+    return None
+
+
+bookstr = ["animals", 'Expeditions-Disasters-and-Adventures', 'Young-Adult', 'Espionage']
+queries = {
+    'snow': 'https://api.nytimes.com/svc/books/v3/lists/current/' + bookstr[0] + '.json?api-key=GUyidHNuX60WGmxjB9I7lSpG7KTdYqtf',
+    'rain': 'https://api.nytimes.com/svc/books/v3/lists/current/' + bookstr[1] + '.json?api-key=GUyidHNuX60WGmxjB9I7lSpG7KTdYqtf',
+    'sun': 'https://api.nytimes.com/svc/books/v3/lists/current/' + bookstr[2] + '.json?api-key=GUyidHNuX60WGmxjB9I7lSpG7KTdYqtf',
+    'clouds': 'https://api.nytimes.com/svc/books/v3/lists/current/' + bookstr[3] + '.json?api-key=GUyidHNuX60WGmxjB9I7lSpG7KTdYqtf'
+}
+
+class book():
+    def __init__(self, booksdata):
+        self.title = booksdata['title']
+        self.author = booksdata['author']
+        self.image = booksdata['book_image']
+        self.description = booksdata['description']
+
+    def __str__(self):
+        return str(self.image + '\n' + self.title + '\n' + self.author + '\n' + self.description)
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('main.html')
+
+@app.route("/results")
+def results():
+    value = request.args.get('weather')
+    bookrequest = urllib.request.urlopen(queries[value])
+    bookdict = json.load(bookrequest)
+    bookdictlist = [book(x) for x in bookdict['results']['books']]
+    booklist = []
+    randomlist = random.sample(range(10), 5)
+    for num in randomlist:
+        booklist.append(bookdictlist[num])
+    return render_template('results.html', value=value, booklist=booklist)
+
+
+app.run(host='localhost', port=8000, debug=True)
+
